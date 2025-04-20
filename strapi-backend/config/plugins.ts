04ad91fef0ff5d600ccb853c8585d27736db481e
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2025-04-20 10:42:25 +0100

    configure sendgrid email provider

--- strapi-backend/config/plugins.ts
+++ strapi-backend/config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"), // Required
+      },
+      settings: {
+        defaultFrom: env(""),
+        defaultReplyTo: "theodore.onyejiaku.g20@gmail.com",
+      },
+    },
+  },
+});
